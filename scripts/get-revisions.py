5c42d6567bb483bd364484edc8a5951c18a22a91
Mars <mfogels@gmail.com>
2019-03-04 12:15:34 -0500

    Screen Phabricator revisions created by scripts

--- scripts/get-revisions.py
+++ scripts/get-revisions.py
@@ -18,7 +18,16 @@ else:
 # The mozilla-central repository object ID in Phabricator.
 MOZILLA_CENTRAL_REPO_ID = "PHID-REPO-saax4qdxlbbhahhp2kg5"
 
+AUTHORS_BLACKLIST = [
+    # We want to skip all revisions created by the "gfx" user.  The account
+    # is used by the Mozilla Graphics team to run scripts that import sourcecode
+    # from GitHub.  The reviews created are automated and no good for reporting
+    # purposes.
+    "PHID-USER-mdjzlkaguw6tdqb256gl",
+]
+
 count = 0
+skipped_by_blacklist = 0
 after = None    # Cursor indicating the start of the next results page.
 
 with open(outfile_name, "w") as fp:
@@ -30,8 +39,15 @@ with open(outfile_name, "w") as fp:
             after=after,
         )
         revisions = response["data"]
-        count += len(revisions)
-        fp.writelines(json.dumps(r) + "\n" for r in revisions)
+
+        for revision in revisions:
+            author = revision["fields"]["authorPHID"]
+            if author in AUTHORS_BLACKLIST:
+                skipped_by_blacklist += 1
+                continue
+
+            count += 1
+            fp.write(json.dumps(revision) + '\n')
 
         cursor = response["cursor"]
         after = cursor["after"]
@@ -40,5 +56,6 @@ with open(outfile_name, "w") as fp:
             # There are no more records.
             break
 
+print(f"Skipped {skipped_by_blacklist} records by blacklisted authors")
 print(f"Wrote {count} records to {outfile_name}")
 sys.exit(0)
